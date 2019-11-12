282f9ed8443809a3ac541478a388dd26d046184d
cabia-anjo_sd2023@CEACCS.NET <cabia-anjo_sd2023@CEACCS.NET>
2019-11-12 16:35:21 +0800

    adding axios

---
+++ subscriber/src/components/Content.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card>
+      <v-card-title>{{event.name}}</v-card-title>
+      <v-image></v-image>
+      <v-card-subtitle>{{event.address}}</v-card-subtitle>
+      <v-card-text>{{event.description}}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- subscriber/src/components/Form.vue
+++ subscriber/src/components/Form.vue
@@ -4,24 +4,32 @@
                 <center>
                     <v-img :src="require('@/assets/logos.png')" id="image"></v-img>
                 </center>
-                <form id="form">
+                <form id="form" @submit.prevent="subscribe" v-show="!isSubscribe" >
                     <v-container>
                         <v-text-field v-model="user.username" :rules="[rules.required]" label="Username"></v-text-field>
                         <v-text-field v-model="user.email" :rules="[rules.required, rules.email]" label="E-mail"></v-text-field>
                         <v-text-field v-model="user.address" :rules="[rules.required]" label="Address"></v-text-field>
                         <v-card-actions>
                             <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" :disabled="!formIsValid" text @click="checkform">Join</v-btn>
+                            <v-btn color="blue darken-1" :disabled="!formIsValid"  text @click="checkform">Join</v-btn>
                         </v-card-actions>
                     </v-container>
                 </form><br />
+                <v-card v-show="isSubscribe"  class="mx-auto" max-width="344">
+                        <v-card-text>
+                            Thank you For Subscribing
+                        </v-card-text>
+                </v-card>
             </v-container>
         </v-card>
 </template>
 <script>
+import axios from "axios";
+
 export default {
     data() {
         return {
+            isSubscribe: false,
             user: {
                 username: '',
                 email: '',
@@ -39,9 +47,22 @@ export default {
     methods: {
         checkform: function (e) {
             if (this.email !== null && this.password !== null) {
-                this.dialog = false;
+                this.isSubscribe = true;
             }
             e.preventDefault();
+        },
+        subscribe: function(){
+             const formData = new FormData();
+             let checkForm = this.checkform();
+             if(checkForm){
+                 formData.append("username", this.user.username)
+                 formData.append("email", this.user.email)
+                 formData.append("address", this.user.address)
+
+                 axios.post("/subscribe", formData).then(res=>{
+                     localStorage.setItem("user", JSON.stringify(res.data.user));
+                 })
+             }
         }
     },
      computed: {
